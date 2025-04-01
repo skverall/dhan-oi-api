3939240d955a828c860dbc5af6b1b001b5d9b597
AydMaxx <167026061+skverall@users.noreply.github.com>
2025-04-01 19:39:41 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 import json
 
@@ -35,42 +34,4 @@ def get_config():
         if tickers:
             config["tickers"] = tickers
     
-=======
-import os
-import json
-
-def get_config():
-    # Приоритет: переменные окружения > config.json
-    config = {}
-    
-    # Чтение из файла config.json
-    if os.path.exists("config.json"):
-        with open("config.json") as f:
-            config = json.load(f)
-    
-    # Переопределение из переменных окружения
-    if os.environ.get("DHAN_TOKEN"):
-        config["token"] = os.environ.get("DHAN_TOKEN")
-    
-    if os.environ.get("DHAN_CLIENT_ID"):
-        config["client_id"] = os.environ.get("DHAN_CLIENT_ID")
-    
-    if os.environ.get("DHAN_AUTH_TYPE"):
-        config["auth_type"] = int(os.environ.get("DHAN_AUTH_TYPE"))
-    
-    # Настройка тикеров через переменные окружения (формат: SYMBOL:EXCHANGE:SECURITY_ID,...)
-    if os.environ.get("DHAN_TICKERS"):
-        tickers = []
-        for ticker_str in os.environ.get("DHAN_TICKERS").split(","):
-            parts = ticker_str.split(":")
-            if len(parts) == 3:
-                tickers.append({
-                    "symbol": parts[0],
-                    "exchange_segment": parts[1],
-                    "security_id": int(parts[2])
-                })
-        if tickers:
-            config["tickers"] = tickers
-    
->>>>>>> e05c2fa (Initial commit)
     return config 
